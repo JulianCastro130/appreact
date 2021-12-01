485ad930a57f838640da499e72289679c56d8f9d
Julian Castro <cococastro619@gmail.com>
2021-12-01 00:00:39 -0300

    Cambios items

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 export const Item = ({ id, name, category, image, price }) => {
   return (
     <Link to={`/product/${id}`}>
-      <div className={`item_card ${category}-category`}>
-        <span>{name}</span>
+      <div className={`card ${category}-category`}>
+        <div className="card-body">
+        <span className="card-title">{name}</span>
         <span>Cat: {category}</span>
         <span>
-          <img src={image} alt={name} className={'small-img'} />
+          <img src={image} alt={name} className={'card-img-top'} />
         </span>
         <span>$ {price}</span>
+        </div>
       </div>
     </Link>
   );
